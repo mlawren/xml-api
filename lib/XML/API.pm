mlawren/xml-api

commit 074566ad5c66affe350a95bf2b856b8651835057

General     Moved syntax tree implementation away from Tree::Simple and into     XML::API::Element. Resulted in large amount of code reduction and     complexity:
        lib/XML/API.pm | 154 insertions(+), 351 deletions(-)

    and also improved the speed considerably for adding elements:

        Benchmark: timing 10000 iterations of new, old...
            new:  2 wallclock secs ( 2.04 usr +  0.24 sys =  2.28 CPU)
                  @ 4385.96/s (n=10000)
            old: 10 wallclock secs ( 8.82 usr +  1.06 sys =  9.88 CPU)
                  @ 1012.15/s (n=10000)

    The speed difference on any non-trivially sized document is
    almost a factor of 2! Rendering has a similar improvement.

Build.PL
    Removed.

XML::API::Element::Cdata, XML::API::Element::Comment
    Folded into XML::API::Element

Makefile.PL, debian/control
    Removed dependencies on Tree::Simple and libtree-simple-perl.

Makefile.PL
@@ -4,7 +4,6 @@ use inc::Module::Install;
 name            'XML-API';
 license         'GPL';
 all_from        'lib/XML/API.pm';
-requires        'Tree::Simple'      => '0.17';
 requires        'XML::Parser::Expat' => 0; 
 build_requires  'Test::More'        => '0.42';
     

debian/control
@@ -8,7 +8,7 @@ Standards-Version: 3.6.1
 
 Package: libxml-api-perl
 Architecture: all
-Depends: ${perl:Depends}, ${misc:Depends}, libtree-simple-perl (>= 1.17-1), libxml-parser-perl
+Depends: ${perl:Depends}, ${misc:Depends}, libxml-parser-perl
 Description:  XML::API - Perl extension for creating XML documents
  XML::API is a class for creating XML documents using object method
  calls. This class is meant for generating XML programatically and not

Build.PL
@@ -1,1 +1,0 @@
-require 'Makefile.PL';

t/01-Element.t
@@ -1,0 +1,54 @@
+use strict;
+use warnings;
+use Test::More tests => 11;
+BEGIN {
+    use_ok('XML::API');
+}
+
+can_ok('XML::API::Element', qw(
+    new
+    add
+));
+
+
+my $e = XML::API::Element->new(
+    element => 'e',
+);
+isa_ok($e, 'XML::API::Element');
+is($e->as_string, '<e />', 'single tag');
+
+$e->add('content');
+is($e->as_string, '<e>content</e>', 'single tag with content');
+
+
+my $c = XML::API::Element->new(
+    comment => 'comment --',
+);
+isa_ok($c, 'XML::API::Element');
+is($c->as_string, '<!-- comment - - -->', 'comment');
+
+
+my $cdata = XML::API::Element->new(
+    cdata => 'cdata',
+);
+isa_ok($cdata, 'XML::API::Element');
+is($cdata->as_string, '<![CDATA[cdata]]>', 'cdata');
+
+
+my $c1 = XML::API::Element->new(
+    element => 'c1',
+);
+$c1->add('cn1');
+
+$e->add($c1);
+
+is($e->as_string('', '  '), 
+'<e>content
+  <c1>cn1</c1>
+</e>', 'combined');
+
+is($e->as_string('', ''), 
+'<e>content
+<c1>cn1</c1>
+</e>', 'combined no indent');
+

t/XML-API-XHTML.t
@@ -2,7 +2,7 @@
 
 use strict;
 use warnings;
-use Test::More tests => 20;
+use Test::More tests => 19;
 use Test::Exception;
 
 BEGIN {
@@ -47,7 +47,6 @@ can_ok($x, '_cdata');
 can_ok($x, '_javascript');
 can_ok($x, '_as_string');
 can_ok($x, '_fast_string');
-can_ok($x, '_print');
 
 $x->_comment('COMMENT');
 

t/XML-API.t
@@ -1,50 +1,80 @@
-#!/usr/bin/perl
-
 use strict;
 use warnings;
-use Test::More tests => 23;
+use Test::More tests => 11;
 use Test::Exception;
 
 BEGIN {
     use_ok('XML::API');
 }
 
-# check that we have a constructor
-can_ok("XML::API", 'new');
+# test the interface
+
+can_ok('XML::API', qw/
+    _encoding
+    _debug
+    _add
+    _parse
+    _current
+    _set_id
+    _goto
+    _attrs
+    _set_lang
+    _langs
+    _cdata
+    _javascript
+    _as_string
+    _fast_string
+/);
+
 
 # giving invalid doctype with new
 throws_ok {
     XML::API->new(doctype => 'notexist');
 } qr/^Could not load module 'XML::API::NOTEXIST'/;
 
-# giving doctype with new
-#throws_ok {
-#    XML::API->new(doctype => 'xhtml', encoding => 'UTF-8');
-#} qr/^Must not specify doctype/;
-
 
 # make a root for our tree
-my $x = XML::API->new(doctype => 'xhtml', encoding => 'UTF-8');
+my $x = XML::API->new;
 isa_ok($x, 'XML::API');
-isa_ok($x, 'XML::API::XHTML');
 
-# test the interface
+$x->e_open();
+is($x, '<?xml version="1.0" encoding="UTF-8" ?>
+<e />', 'e content');
 
-can_ok($x, '_encoding');
-can_ok($x, '_debug');
-can_ok($x, '_add');
-can_ok($x, '_parse');
-can_ok($x, '_current');
-can_ok($x, '_set_id');
-can_ok($x, '_goto');
-can_ok($x, '_attrs');
-can_ok($x, '_set_lang');
-can_ok($x, '_langs');
-can_ok($x, '_cdata');
-can_ok($x, '_javascript');
-can_ok($x, '_as_string');
-can_ok($x, '_fast_string');
-can_ok($x, '_print');
+$x->c('content');
+is($x, '<?xml version="1.0" encoding="UTF-8" ?>
+<e>
+  <c>content</c>
+</e>', 'e c content');
+
+my $n = XML::API->new;
+$n->n_open;
+$n->n2_open('content');
+$n->n3;
+
+is($n, '<?xml version="1.0" encoding="UTF-8" ?>
+<n>
+  <n2>content
+    <n3 />
+  </n2>
+</n>', 'n content');
+
+$x->_add($n);
+
+is($x, '<?xml version="1.0" encoding="UTF-8" ?>
+<e>
+  <c>content</c>
+  <n>
+    <n2>content
+      <n3 />
+    </n2>
+  </n>
+</e>', 'e c n content');
+
+
+use Data::Dumper;
+$Data::Dumper::Indent = 1;
+#die Dumper($x);
 
 $x->_parse('<div><p>text</p></div>');
 $x->_comment('COMMENT');

t/langs.t
@@ -1,27 +1,28 @@
-#!/usr/bin/perl
-
 use strict;
 use warnings;
-use Test::More tests => 7;
+use Test::More tests => 6;
 use Test::Exception;
 use XML::API;
 
 my $x = XML::API->new(doctype => 'xhtml');
 
-$x->html_open;
+$x->html_open(undef);
 $x->_set_lang('en');
 
-ok($x->_langs == 1, 'first lang accepted');
+ok($x->_langs == 1, 'first lang recorded');
+is($x,'<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xml:lang="en" xmlns="http://www.w3.org/1999/xhtml"></html>','html ok');
 
 $x->_set_lang('de');
-
+$x->head('test head');
 ok($x->_langs == 2, 'second lang accepted');
 
-$x->head('test head');
-my $str = $x->_as_string;
-
-ok($str =~ /html.*xml:lang="en"/, 'html is en');
-ok($str =~ /head.*xml:lang="de"/, 'head is de');
+is($x,'<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+  <head xml:lang="de">test head</head>
+</html>','html ok');
 
 
 #
@@ -33,7 +34,10 @@ $x->_set_lang('en');
 $x->two('a test two element');
 
 ok($x->_langs == 1, 'lang accepted for generic xml');
-$str = $x->_as_string;
+is($x,'<?xml version="1.0" encoding="UTF-8" ?>
+<one>
+  <two xml:lang="en">a test two element</two>
+</one>','generic xml ok');
 
-ok($str !~ /one.*xml:lang="en"/, 'root node lang is not set');
-ok($str =~ /two.*xml:lang="en"/, 'two did get the language');
+
+

commit 136ffe8c4e1f08c6dd561fe407146a52b5f2bdba

XML::API     Bugfix: Removed circular-reference memory leak in XML::API::Element     which was introduced by the rework in     074566ad5c66affe350a95bf2b856b8651835057
Makefile.PL, debian/control
    New dependency on Test::Memory::Cycle and libtest-memory-cycle-perl.

CPAN
    Uploaded as v0.16.

Makefile.PL
@@ -7,6 +7,7 @@ all_from        'lib/XML/API.pm';
 requires        'XML::SAX'            => 0; 
 build_requires  'Test::More'          => '0.42';
 build_requires  'Test::Exception'     => 0;
+build_requires  'Test::Memory::Cycle' => 0;
 build_requires  'Test::Pod'           => 0;
 build_requires  'Test::Pod::Coverage' => 0;
     

debian/control
@@ -1,7 +1,7 @@
 Source: libxml-api-perl
 Section: perl
 Priority: optional
-Build-Depends: debhelper (>= 4.0.2), libmodule-build-perl, libtest-exception-perl, libtest-pod-perl, libtest-pod-coverage-perl
+Build-Depends: debhelper (>= 4.0.2), libmodule-build-perl, libtest-exception-perl, libtest-pod-perl, libtest-pod-coverage-perl, libtest-memory-cycle-perl
 Build-Depends-Indep: perl (>= 5.8.0-7)
 Maintainer: mlawren <nomad@null.net>
 Standards-Version: 3.6.1

lib/XML/API.pm
@@ -3,6 +3,7 @@ package XML::API::Element;
 use strict;
 use warnings;
 use Carp qw(croak);
+use Scalar::Util qw(weaken);
 
 our $VERSION = '0.16';
 
@@ -19,6 +20,8 @@ sub new {
     if ($self->{comment}) {
         $self->{comment} =~ s/--/- -/g;
     }
+
+    weaken($self->{parent}) if (exists $self->{parent});
 
     bless ($self, $class);
     return $self;

t/03-XML-API.t
@@ -1,7 +1,8 @@
 use strict;
 use warnings;
-use Test::More tests => 12;
+use Test::More tests => 13;
 use Test::Exception;
+use Test::Memory::Cycle;
 
 BEGIN {
     use_ok('XML::API');
@@ -142,3 +143,6 @@ is($a, '<?xml version="1.0" encoding="UTF-8" ?>
   <label>Body</label>
   <textarea cols="50" name="body" rows="10">the body</textarea>
 </p>', 'Abstract syntax tree input');
+
+
+memory_cycle_ok($x, 'memory cycle');